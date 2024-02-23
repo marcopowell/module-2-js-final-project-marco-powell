e3c1692652971108358f08e53bd22806189ca067
Marco Powell <mvapowell@gmail.com>
2024-02-22 22:41:59 -0600

    created html skeleton and basic structure including buttons and game area

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const gameContainer = document.getElementById('game-container');
+const playerFigure = document.getElementById('figures');
+
+
+// Game container 
+// gameContainer.innerHTML = '<p>This is the game area</p>'
+gameContainer.style.height = '70vh';
+gameContainer.style.width = '80vw';
+gameContainer.style.backgroundColor = 'rgb(200, 216, 74)';
+gameContainer.style.display = 'flex';
+gameContainer.style.flexDirection = 'column';
+gameContainer.style.justifyContent = 'center';
+gameContainer.style.alignItems = 'center';
+
+// Figure image 
+playerFigure
+
 console.log('This is hangman');
